36bb528a2be2c5b65ffbffc9ff031ca2eceb6fc6
Carlos Segura <carlosandres.segurac@gmail.com>
2023-06-28 19:56:05 -0500

    delete import

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,12 +1,10 @@
 import style from '../pages/index.module.css'
-import styleBtn from '../components/Buttons/Buttons.module.css'
 
 export default function AboutMe () {
   return (
     <div id='sobremi' className={style.containerAbout}>
     <p>Diseñador y programador en <b className={style.javascript}>JavaScript</b> con <b>1 año</b> de experiencia enfocado en la creación de soluciones digitales enfocadas al <b>UI/UX</b>.</p>
       <div className={style.containerRoadMap}>
-        {/* <button className={styleBtn.btnCommonS}>Mi roadmap {'>'}</button> */}
       </div>
     </div>
   )
